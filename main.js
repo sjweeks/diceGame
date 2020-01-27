a7df532347c620103a17f7e8dab57004e5333bfb
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-01-27 15:25:58 +0000

    New Commit

--- main.js
+++ main.js
@@ -27,38 +27,53 @@ const playerScore = document.getElementById("playerScore");
 const diceImage = document.getElementById("diceImage");
 const totalScore = document.getElementById("totalScore");
 const winStatus = document.getElementById("winStatus");
+// const restart = document.getElementById("playButton");
 
 diceImage.style.visibility = "hidden";
 
+// let startGame;
 let numberRolled;
 let pointScored = 0;
 
 //function declaration
 
+
+const restart = () => {
+    pointScored = 0;
+    winStatus.style.visibility = "";
+    winStatus.textContent = "";
+    diceImage.style.visibility = "hidden";
+    winStatus.textContent = "";
+    totalScore.textContent = "";
+    playerScore.textContent = "";
+    rollButton = "";
+}
+
+
 const diceRoll = () => {
-    numberRolled = (Math.ceil(Math.random()*6));
+    numberRolled = (Math.ceil(Math.random() * 6));
     console.log(numberRolled);
 }
 
 // diceRoll()
 
 const winOrLose = () => {
-    // numberRolled = diceRoll();
-    // pointScored = scoreAddition();
-        if (numberRolled == 1){
-            winStatus.textContent = ("You have lost, try again.")
-            pointScored = 0;
-            // diceImage.style.visibility = "hidden";
-        }
-        else if (pointScored >= 20) {
-            winStatus.textContent = ("You win!")
-            pointScored = 0;
-            // diceImage.style.visibility = "hidden";
-        }
-        else {
-            winStatus.textContent = ("Have another roll")
-        }
-        scoreAddition();
+    
+    if (numberRolled == 1) {
+        winStatus.textContent = ("You have lost, play again?.")
+        pointScored = 0;
+        // restart()
+        
+    }
+    else if (pointScored >= 20) {
+        winStatus.textContent = ("You win!")
+        pointScored = 0;
+        // restart()
+    }
+    else {
+        winStatus.textContent = ("Have another roll")
+    }
+    scoreAddition();
 }
 
 const scoreAddition = () => {
@@ -72,14 +87,32 @@ rollButton.addEventListener("click", () => {
     winOrLose()
     playerScore.textContent = numberRolled;
     diceImage.style.visibility = "visible";
-    diceImage.src = `images/dice${numberRolled}.png`
+    diceImage.src = `img/dice${numberRolled}.png`
     // console.log("Hello there");
     // pointScored += numberRolled;
     totalScore.textContent = (`Your total score is ${pointScored}`)
     // scoreAddition();
-
 })
 
+playButton.addEventListener("click", () => {
+    restart();
+})
+
+
+
+
+// function restart() {
+//     diceRoll()
+//     winOrLose()
+//     playerScore.textContent = numberRolled;
+//     diceImage.style.visibility = "visible";
+//     diceImage.src = `images/dice${numberRolled}.png`
+//     // console.log("Hello there");
+//     // pointScored += numberRolled;
+//     totalScore.textContent = (`Your total score is ${pointScored}`)
+//     // scoreAddition();
+
+// }
 
 // winOrLose();
 
