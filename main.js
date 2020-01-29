bd68810bcee0df30b0547a92a4b9de4ccf65d778
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-01-29 18:25:15 +0000

    New Commit

--- main.js
+++ main.js
@@ -27,6 +27,12 @@ const playerScore = document.getElementById("playerScore");
 const diceImage = document.getElementById("diceImage");
 const totalScore = document.getElementById("totalScore");
 const winStatus = document.getElementById("winStatus");
+const rolled1 = document.getElementById("stats1");
+const rolled2 = document.getElementById("stats2");
+const rolled3 = document.getElementById("stats3");
+const rolled4 = document.getElementById("stats4");
+const rolled5 = document.getElementById("stats5");
+const rolled6 = document.getElementById("stats6");
 // const restart = document.getElementById("playButton");
 
 diceImage.style.visibility = "hidden";
@@ -34,6 +40,12 @@ diceImage.style.visibility = "hidden";
 // let startGame;
 let numberRolled;
 let pointScored = 0;
+let no1 = 0;
+let no2 = 0;
+let no3 = 0;
+let no4 = 0;
+let no5 = 0;
+let no6 = 0;
 
 //function declaration
 
@@ -87,18 +99,44 @@ rollButton.addEventListener("click", () => {
     winOrLose()
     playerScore.textContent = numberRolled;
     diceImage.style.visibility = "visible";
-    diceImage.src = `img/dice${numberRolled}.png`
+    diceImage.src = `img/dice${numberRolled}.png`;
+    incrementFunction()
     // console.log("Hello there");
     // pointScored += numberRolled;
     totalScore.textContent = (`Your total score is ${pointScored}`)
-    // scoreAddition();
 })
 
 playButton.addEventListener("click", () => {
     restart();
 })
 
-
+const incrementFunction = () => {
+    // diceRoll()
+    if (numberRolled == 1){
+        no1++
+        rolled1.textContent = no1;
+    }
+    else if (numberRolled == 2){
+        no2++
+        rolled2.textContent = no2;
+    }
+    else if (numberRolled == 3){
+        no3++
+        rolled3.textContent = no3;
+    }
+    else if (numberRolled == 4){
+        no4++
+        rolled4.textContent = no4;
+    }
+    else if (numberRolled == 5){
+        no5++
+        rolled5.textContent = no5;
+    }
+    else if (numberRolled == 6){
+        no6++
+        rolled6.textContent = no6;
+    }
+}
 
 
 // function restart() {
